b724ef10ea37094c092bfe40d491ac753c2e4798
Mateus Ribeiro NM <mateusmt676@gmail.com>
2025-08-08 19:13:57 -0400

    upload

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,13 +3,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Início</a>
+          <a class="nav-link active" aria-current="page" href="/NumBases/">Início</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/ferramenta">Ferramenta</a>
+          <a class="nav-link active" aria-current="page" href="/NumBases/ferramenta">Ferramenta</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/sobre">Sobre o autor</a>
+          <a class="nav-link active" aria-current="page" href="/NumBases/sobre">Sobre o autor</a>
         </li>
       </ul>
     </div>
